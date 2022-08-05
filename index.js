b893323d1e11e64510a06e5978c12f6904e36132
YonnathaDev <yonnathan.placencia.dev>
2022-08-05 19:26:52 -0400

    env mod 4

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ app.use( express.json() );
 app.use( '/api/auth', require('./routes/auth') );
 
 // Manejar demás rutas
-// app.get('*', (req, res) => {
-//     res.sendFile( path.resolve( __dirname, 'public/index.html') );
-// })
+app.get('*', (req, res) => {
+    res.sendFile( path.resolve( __dirname, 'public/index.html') );
+})
 
 
 app.listen( process.env.PORT , () => {
